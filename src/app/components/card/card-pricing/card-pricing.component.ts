eb973410a8aa18f19fa3375d9324485ebc6f78f9
Mello <william.mello@capgemini.com>
2024-01-10 12:13:43 -0300

    final

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-pricing',
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './card-pricing.component.html',
   styleUrl: './card-pricing.component.css'
 })
-export class CardPricingComponent {
 
+export class CardPricingComponent implements 
+OnInit{
+  @Input()
+  gameType:string ="Digital PS4"
+  @Input()
+  gamePrice:string = "R$ 399,90"
+  constructor(){}
+
+  ngOnInit():void {}
 }
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CardLabelComponent } from "./card-label/card-label.component";
+import { CardPricingComponent } from "./card-pricing/card-pricing.component";
 
 @Component({
-  selector: 'app-card',
-  standalone: true,
-  imports: [],
-  templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+    selector: 'app-card',
+    standalone: true,
+    templateUrl: './card.component.html',
+    styleUrl: './card.component.css',
+    imports: [CardLabelComponent, CardPricingComponent]
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+    @Input()
+    gameCover:string=""
+
+    @Input()
+    gameLabel: string=""
+
+    @Input()
+    gameType:string ="Digital PS4"
+
+    @Input()
+    gamePrice:string = "R$ 399,90"
+    
+    constructor(){}
+
+    ngOnInit(): void {
+        
+    }
 
 }
